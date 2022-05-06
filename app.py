f7a1bd39fdda31d697246f0c71ac9f235f97182a
yuta sakanaka <saka_m1@saka-m1.local>
2022-05-06 19:51:23 +0900

    getの雛形作成．filter_byでallとid指定を分岐させる関数作成予定

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ class Article(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     name = db.Column(db.String(80))
     article = db.Column(db.Text())
-    now_date = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    now_date = db.Column(db.DateTime, nullable=False, default=datetime.now())
     thread_id = db.Column(db.Integer, db.ForeignKey('thread.id'), nullable=False)
     thread = db.relationship('Thread',
         backref=db.backref('articles', lazy=True))
@@ -27,7 +27,7 @@ class Article(db.Model):
 class Thread(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     thread_name = db.Column(db.String(80), unique=True, nullable=False)
-    post_date = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    post_date = db.Column(db.DateTime, nullable=False, default=datetime.now())
 
     def __repr__(self):
         return '<Thread %r>' % self.thread_name
@@ -42,7 +42,6 @@ def index():
 def thread_detail_show(title):
     title = Thread.query.filter_by(thread_name=title).first()
     articles = Article.query.filter_by(thread_id=title.id).all()
-    
     return render_template('thread.html',
                             thread=title.thread_name,
                             articles=articles)
@@ -59,7 +58,7 @@ def article_add():
     return redirect(url_for('thread_detail_show', title=res["thread"]))
 
 @app.route("/create_thread", methods=["POST"])
-def thread_create():
+def create_thread():
     thread_get = request.form["thread"]
     thread_list = []
     threads = Thread.query.all()
@@ -68,8 +67,31 @@ def thread_create():
         thread_new = Thread(thread_name=thread_get)
         db.session.add(thread_new)
         db.session.commit()
+#    thread_id = Thread.query.filter_by(thread_name=thread_get).first()
     return redirect(url_for('thread_detail_show', title=thread_get))
 
+@app.route("/api/get_thread", methods=["GET"])
+def get_thread_infomation():
+    req = request.args
+    api_thread_id = req.get("thread_id")
+
+    if api_thread_id is None:
+        thread_all_get = Thread.query.all()
+        thread_list = []
+        for thread in thread_all_get:
+            thread_detail = {"thread_name": thread.thread_name, "id": thread.id, "date": thread.post_date}
+            thread_list.append(thread_detail)
+        return jsonify(thread_list)
+
+    else:
+        data = {api_thread_id: []}
+        thread_api_get = Thread.query.filter_by(id=api_thread_id)
+        articles = Article.query.filter_by(thread_id=api_thread_id).all()
+        for article in articles:
+            article_detail = {"name": article.name, "article": article.article, "date": article.now_date}
+            data[api_thread_id].append(article_detail)
+
+        return jsonify(data)
 
 if __name__ == "__main__":
     app.run()
